a83aa4f9bdabce01462f7d811f098267d44971bc
jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-01-20 21:45:16 +0000

    added colour to timeblock script.js

--- starter/script.js
+++ starter/script.js
@@ -3,3 +3,16 @@ $(document).ready(function () {
     var currentDay = moment().format("dddd, MMMM Do YYYY");
     $("#currentDay").text("Today is: " + currentDay);
 });
+
+//To color-code each timeblock based on past, present, and future, you can use moment.js to compare the current time to the time of each timeblock. 
+$(".container").find("div").each(function () {
+    var blockHour = moment($(this).text(), "h a").format("H");
+    var currentHour = moment().format("H");
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+});
